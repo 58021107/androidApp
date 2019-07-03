f276c0adf4cb69f6bf9e96479001b79a8540074b
Nuttamon_CNP <nuttamon.piluek@gmail.com>
2019-07-03 15:10:14 +0700

    file project

--- app/src/main/java/com/example/administrator/power/farming.java
+++ app/src/main/java/com/example/administrator/power/farming.java
@@ -1,12 +1,18 @@
+
 package com.example.administrator.power;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
@@ -14,6 +20,7 @@ import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -30,125 +37,86 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class farming extends AppCompatActivity {
+public class farming extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    TextView tv;
-    Button btn;
-    Calendar cal;
+
+    Button selectDate;
+    TextView date;
     DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
 
     private RequestQueue mQueue;
-    List<String> farming_lists = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farming);
 
-        tv = (TextView)findViewById(R.id.tv);
-        btn = (Button)findViewById(R.id.btn);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        selectDate = findViewById(R.id.btnDate);
+        date = findViewById(R.id.tvSelectedDate);
+
+        selectDate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                cal = Calendar.getInstance();
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-                int month = cal.get(Calendar.MONTH);
-                int year = cal.get(Calendar.YEAR);
+            public void onClick(View view) {
 
-                datePickerDialog = new DatePickerDialog(farming.this, new DatePickerDialog.OnDateSetListener(){
-                    @Override
-                    public void onDateSet (DatePicker datePicker,int mYear,int mMonth,int mDay){
-                        tv.setText(mDay + "/" + (mMonth + 1) + "/" + mYear);
-                    }
-                },day,month,year);
-
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(farming.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                date.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.show();
             }
         });
-//        ListView listView = findViewById(R.id.farming_list);
+
+
         mQueue = Volley.newRequestQueue(this);
 
-//        getJson(listView);
 
-        List<String> list = new ArrayList<String >();
-        list.add("กข5");
-        list.add("กข6");
-        list.add("กข7");
-        list.add("กข8");
+        Spinner spinner3 = findViewById(R.id.spinner3);
+        Spinner spinner4 = findViewById(R.id.spinner4);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.Breed, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
+                R.array.Cultivate, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+        spinner3.setOnItemSelectedListener(this);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(adapter4);
+        spinner4.setOnItemSelectedListener(this);
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayShowHomeEnabled(true);
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
-        Spinner spinner = (Spinner)this.findViewById(R.id.spinner3);
-        spinner.setAdapter((dataAdapter));
-
-//        List<String> listView = new ArrayList<String >();
-//        listView.add("นาดำ");
-//        listView.add("นาหว่าน");
-//
-//        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
-//        Spinner spinnerlist = (Spinner)this.findViewById(R.id.spinner4);
-//        spinner.setAdapter((dataAdapter));
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(farming.this,suggestion.class);
+                startActivity(intent);
+            }
+        });
+        //ปุ่มแสดงคำแนะนำ
     }
-//
-//    public void getJson(final ListView listView) {
-//
-//        String url = "http://10.0.2.2/project/data/farming.php";
-//
-//        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
-//                new Response.Listener<JSONArray>() {
-//                    @Override
-//                    public void onResponse(JSONArray response) {
-//                        try {
-//                            for (int i = 0; i < response.length(); i++) {
-//                                JSONObject rice = response.getJSONObject(i);
-//
-//                                String  rice_id = rice.getString("rice_id");
-//                                String rice_name = rice.getString("rice_name");
-//
-//                                farming_lists.add(rice_id);
-//                            }
-//
-//                            RiceApdapter adapter = new RiceApdapter();
-//                            listView.setAdapter(adapter);
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                error.printStackTrace();
-//            }
-//        });
-//
-//        mQueue.add(request);
-//    }
 
-//    class RiceApdapter extends BaseAdapter {
-//
-//        @Override
-//        public int getCount() {
-//            return farming_lists.size();
-//        }
-//
-//        @Override
-//        public Object getItem(int position) {
-//            return null;
-//        }
-//
-//        @Override
-//        public long getItemId(int position) {
-//            return 0;
-//        }
-//
-//        @SuppressLint("ViewHolder")
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//            convertView = getLayoutInflater().inflate(R.layout.farming_row, null);
-//
-//            Button name_list = convertView.findViewById(R.id.farming);
-//
-//            name_list.setText(farming_lists.get(position));
-//            return convertView;
-//        }
-//    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//        String text = parent.getItemIdAtPosition(position).toString();
+//        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
--- app/src/main/java/com/example/administrator/power/page1.java
+++ app/src/main/java/com/example/administrator/power/page1.java
@@ -1,11 +1,10 @@
-        package com.example.administrator.power;
+package com.example.administrator.power;
 
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
